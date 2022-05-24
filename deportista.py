dde9ac08cf8be89e550721f069155bd3945024c9
juanseb253 <92771458+juanseb253@users.noreply.github.com>
2022-05-24 12:22:54 -0500

    Add files via upload

--- deportista.py
+++ deportista.py
@@ -1,14 +1,21 @@
 from persona import Persona
+
+
 class Deportista(Persona):
-    def __init__(self, nombre , edad , altura, sexo, deporte, añosPracticando):
-        super.__init__(nombre, edad, altura, sexo)
-        self._deporte = deporte
-        self._añosPracticando = añosPracticando
+    def __init__(self,nombre,edad,altura,sexo,deporte,años):
+        super().__init__(nombre,edad,altura,sexo)
+        self._deporte=deporte
+        self._añosPracticando=años
+
+    
     def getDeporte(self):
         return self._deporte
+    
     def getAñosPracticando(self):
         return self._añosPracticando
-    def setDeporte(self, deporte):
-        self._deporte = deporte
-    def serAñosPracticando(self, años):
-        self._añosPracticando = años
+
+    def setDeporte(self,nuevo):
+        self._deporte=nuevo
+
+    def setAñosPracticando(self,nuevo):
+        self._añosPracticando=nuevo
